f96b03f7b779749c5f8db8e46b4905e2fa83064f
xiaxueyi00-eng <xia.xueyi00@gmail.com>
2026-01-15 14:34:31 -0500

    put cart 263 on the github

---
+++ p5-rev-template/js/script.js
@@ -0,0 +1,25 @@
+"use strict";
+
+function setup() {
+    console.log("go")
+
+    let x, y, w, h;
+    let r, g, b;
+
+    x = 100;
+    y = 100;
+    w = 50;
+    h = 50;
+    r = 255;
+    g = 0;
+    b = 0;
+
+    fill(r, g, b);
+    ellipse(x, y, w, h);
+
+}
+draw
+
+function draw() {
+
+}
