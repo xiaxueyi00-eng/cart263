b423d78a5c8cc0dd016f4e863b782063ca1960db
xiaxueyi00-eng <xia.xueyi00@gmail.com>
2026-01-18 10:09:15 -0500

    finished QU6
    I displayed a centered text on the canvas and using spacing to prevent them from overlapping.

---
+++ p5-rev-template/js/script6.js
@@ -0,0 +1,46 @@
+// ---------------- VARIABLES / CONSTS ----------------
+const TEXT_SIZE = 28;
+const TEXT_COLOR = 255;
+const SPACING = 24;
+
+let textObj = {
+    str: "test",
+    x: 0,
+    y: 0
+};
+
+// ---------------- SETUP ----------------
+function setup() {
+    createCanvas(600, 400);
+
+    textSize(TEXT_SIZE);
+    fill(TEXT_COLOR);
+    textAlign(CENTER, CENTER);
+
+    textObj.x = width / 2;
+    textObj.y = height / 2;
+}
+
+// ---------------- DRAW ----------------
+function draw() {
+    background(0);
+
+    text(textObj.str, textObj.x, textObj.y);
+
+    // ---------- FOR LOOP 1 ----------
+    for (let i = 0; i <= 9; i++) {
+        let xPos = 20 + i * SPACING;
+        let yPos = 20;
+
+        text(i, xPos, yPos);
+    }
+
+    // ---------- FOR LOOP 2 ----------
+
+    for (let i = 1; i <= 15; i++) {
+        let xPos = 20;
+        let yPos = 20 + i * SPACING;
+
+        text(i, xPos, yPos);
+    }
+}
